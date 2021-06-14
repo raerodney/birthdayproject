c155ab3ca88dd1c4de261b494210571d45a764b3
raerodney <rodneykinyua@outlook.com>
2021-06-14 11:05:26 +0300

    Controlled interface

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+const maleAkan = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+
+const femaleAkan = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+
+const week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+const myBirthday = document.getElementById('myBirthdayDate').value;
+
+const myGender = document.getElementsByName('gender');
+
+const dateOfBirth = new Date(myBirthday);
+
+const dayOfWeek = dateOfBirth.getDay();
+
+const i = 0;
+
+if (maleAge()===myBirthday) {
+    `hui`;
+    
+}
